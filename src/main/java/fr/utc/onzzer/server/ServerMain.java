dce5051ed9bcdad0de367e1fcb6fb0c9b572cfb9
ilianAZZ <ilian572@gmail.com>
2023-11-22 23:53:36 +0100

    Removing git submodules. Using maven dependencies

---
+++ src/main/java/fr/utc/onzzer/server/ServerMain.java
@@ -0,0 +1,10 @@
+package fr.utc.onzzer.server;
+
+import fr.utc.onzzer.common.dataclass.User;
+
+public class ServerMain {
+    public static void main(String[] args) {
+        // show that User is correctly imported from onzzer-common
+        User u;
+    }
+}
