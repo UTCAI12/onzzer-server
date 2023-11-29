216122dc9b69d36fcb2b7c201c95a5a0ab26f057
Ilian AZZ <35274812+ilianAZZ@users.noreply.github.com>
2023-11-29 15:51:23 +0100

    Revert "interface"

--- src/main/java/fr/utc/onzzer/server/data/interfaces/DataTrackServices.java
+++
@@ -1,9 +0,0 @@
-import java.util.UUID;
-
-public interface DataTrackServices {
-    void updateTrack(TrackLite track) throws Exception;
-
-    Track getTrack(UUID trackId) throws Exception;
-
-    void removeAllTracks(UUID userUuid) throws Exception;
-}
