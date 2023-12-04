9261826e238897c0da3816ab2ba29eaac9da05c3
maxime <maxime.six@etu.utc.fr>
2023-12-04 10:18:06 +0100

    removing changes on data file

--- src/main/java/fr/utc/onzzer/server/data/DataRepository.java
+++ src/main/java/fr/utc/onzzer/server/data/DataRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.Map;
 
 public class DataRepository {
-    private static final Map<UserLite, List<TrackLite>> usersAndTracks = new HashMap<>();
+    private final Map<UserLite, List<TrackLite>> usersAndTracks = new HashMap<>();
 
-    public static Map<UserLite, List<TrackLite>> getUsersAndTracks() {
+    public Map<UserLite, List<TrackLite>> getUsersAndTracks() {
         return usersAndTracks;
     }
 }
