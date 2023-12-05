38d521263ffb07a9c35c5585ede2bb88b657b10b
Valentin Fagnet <valentin.fagnet@etu.utc.fr>
2023-12-05 19:07:58 +0100

    ~ Changed ServerRequestHandler constructor (removed "users" param) ~ Changed every use of "users" attribute to use dataRepository in ServerRequestHandler - Removed "users" attribute in ServerRequestHandler

--- src/main/java/fr/utc/onzzer/server/communication/ServerRequestHandler.java
+++ src/main/java/fr/utc/onzzer/server/communication/ServerRequestHandler.java
@@ -1,5 +1,6 @@
 package fr.utc.onzzer.server.communication;
 
+import fr.utc.onzzer.common.dataclass.User;
 import fr.utc.onzzer.server.data.ServerController;
 import fr.utc.onzzer.common.dataclass.communication.SocketMessage;
 import fr.utc.onzzer.common.dataclass.communication.SocketMessagesTypes;
@@ -16,64 +17,74 @@ import java.util.*;
 
 public class ServerRequestHandler {
 
-    private final Map<UserLite, ServerSocketManager> users;
-
     private ServerController serverController;
 
-    public ServerRequestHandler(final Map<UserLite, ServerSocketManager> users, ServerController serverController) {
-        this.users = users;
+    public ServerRequestHandler(ServerController serverController) {
         this.serverController = serverController;
     }
 
     public void sendAllExclude(final SocketMessage message, final UUID excluded) {
         System.out.println("Server: send message to all registered users excluded : " + excluded);
-        this.users.forEach((user, serverSocketManager) -> {
-            if (excluded != user.getId()) {
-                try {
-                    serverSocketManager.send(message);
-                } catch (IOException e) {
-                    System.out.println("Server: Not able to reach user, removing it from list");
-                    this.users.remove(user);
+
+        for (UserLite user: this.serverController.getDataUserServices().getAllUsers()) {
+            try {
+                if (excluded != user.getId()) {
+                    try {
+                        this.serverController.getDataUserServices().getSocket(user).send(message);
+                    } catch (IOException e) {
+                        System.out.println("Server: Not able to reach user, removing it from list");
+                        this.serverController.getDataUserServices().deleteUser(user);
+                    }
+                    System.out.println("Server: sending to:" + user.getId());
                 }
-                System.out.println("Server: sending to:" + user.getId());
+            } catch (UserLiteNotFoundException e) {
+                System.out.println("Server: The user does not exists");
             }
-        });
+        }
     }
 
     public void sendMessageToUser(final SocketMessage message, final UserLite user) {
-        ServerSocketManager userSocketManager = users.get(user);
-        if (userSocketManager != null) {
-            try {
-                userSocketManager.send(message);
-                System.out.println("Server: GET_TRACK message sent to user: " + user.getUsername());
-            } catch (IOException e) {
-                System.out.println("Server: Not able to reach user " + user.getUsername());
-                users.remove(user); // Remove the user if we can't reach them
+        try {
+            ServerSocketManager userSocketManager = this.serverController.getDataUserServices().getSocket(user);
+            if (userSocketManager != null) {
+                try {
+                    userSocketManager.send(message);
+                    System.out.println("Server: GET_TRACK message sent to user: " + user.getUsername());
+                } catch (IOException e) {
+                    System.out.println("Server: Not able to reach user " + user.getUsername());
+                    this.serverController.getDataUserServices().deleteUser(user); // Remove the user if we can't reach them
+                }
+            } else {
+                System.out.println("Server: User with ID " + user.getUsername() + " not found.");
             }
-        } else {
-            System.out.println("Server: User with ID " + user.getUsername() + " not found.");
+        } catch (UserLiteNotFoundException e) {
+            System.out.println("Server: The user does not exist");
         }
     }
 
 
     void userConnect(final SocketMessage message, final UserLite userLite, final ServerSocketManager sender) {
         // update the local model with the new user
-        this.users.put(userLite, sender);
+        this.serverController.getDataUserServices().addUser(userLite, sender);
 
         // associate the ClientHandler with the appropriate User
         sender.setUser(userLite);
 
-        System.out.println("Server: new client : "+ userLite.getId() + " ! there is now " + this.users.size() + " registered clients");
+        System.out.println("Server: new client : "+ userLite.getId() + " ! there is now " + this.serverController.getDataUserServices().getAllUsers().size() + " registered clients");
 
         // Notify all users exclude "user" in parameter that a new user is connected (just forwarding the initial message)
         this.sendAllExclude(message, userLite.getId());
 
         // TODO : remove multiple message sending, send only one global message : SocketMessagesTypes.USER_CONNECTED
-        SocketMessage m = new SocketMessage(SocketMessagesTypes.USER_CONNECTED, new ArrayList<>(users.keySet()));
+        SocketMessage m = new SocketMessage(SocketMessagesTypes.USER_CONNECTED, new ArrayList<>(this.serverController.getDataUserServices().getAllUsers()));
         try {
             sender.send(m);
         } catch (IOException e) {
-            this.users.remove(userLite);
+            try {
+                this.serverController.getDataUserServices().deleteUser(userLite);
+            } catch (UserLiteNotFoundException e2){
+                System.out.println("Server: The user does not exist");
+            }
         }
 
         /*
@@ -89,7 +100,12 @@ public class ServerRequestHandler {
 
     void userDisconnect(final SocketMessage message, final UserLite userLite, final ServerSocketManager sender) {
         // removing the user from the local "model"
-        this.users.remove(userLite);
+        try {
+            this.serverController.getDataUserServices().deleteUser(userLite);
+        } catch (UserLiteNotFoundException e) {
+            System.out.println("Server: The user does not exist");
+        }
+
         // notifying others users by forwarding the initial message
         this.sendAllExclude(message, userLite.getId());
     }
